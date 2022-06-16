13a2184cd1cb69b714273d568af2430663a2f636
Bala Subramanian <98680016+balasubbu2327@users.noreply.github.com>
2022-06-17 01:42:14 +0530

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Bala</span>
+          <span className="logo">Balasubbu</span>
         </Link>
       </div>
       <hr />
